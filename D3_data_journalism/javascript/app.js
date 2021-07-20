325d00e6b16e05753b25588aada8057fdd573aa1
bakerv <80018640+bakerv@users.noreply.github.com>
2021-07-20 00:46:07 -0500

    corrected data casting error that was causing scaling issues and implemented new text drawing function for the data points

--- D3_data_journalism/javascript/app.js
+++ D3_data_journalism/javascript/app.js
@@ -1,14 +1,13 @@
 d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
-    rawData.forEach(data => +data);
-
+    console.log(rawData)
     // specify overall plotting parameters
     let svgHeight = 600;
-    let svgWidth = 600;
+    let svgWidth = 960;
 
     let margin = {
         top:50,
         bottom: 50,
-        left: 50,
+        left: 70,
         right: 50
     };
 
@@ -34,16 +33,16 @@ d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
         {smokes: "Active Smokers (%)"}];
 
     // Initial Plot Parameters
-    var xData = Object.keys(menuLabels[1])
-    var yData = Object.keys(menuLabels[1])
-    var xLabel = Object.values(menuLabels[1])
-    var yLabel = Object.values(menuLabels[1])
+    var xData = Object.keys(menuLabels[5])
+    var yData = Object.keys(menuLabels[4])
+    var xLabel = Object.values(menuLabels[5])
+    var yLabel = Object.values(menuLabels[04])
     
     function xAxisScale(dataSet,dataColumn) {
-        let scale = d3.scaleLinear()
+        var scale = d3.scaleLinear()
             .domain([
-                d3.min(dataSet, data => data[dataColumn]) * 0.8,
-                d3.max(dataSet, data => data[dataColumn]) * 1.2])
+                d3.min(dataSet, data => +data[dataColumn]) * 0.8,
+                d3.max(dataSet, data => +data[dataColumn]) * 1.2])
             .range([0,chartWidth]);
         
         return scale;
@@ -52,8 +51,8 @@ d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
     function yAxisScale(dataSet, dataColumn) {
         let scale = d3.scaleLinear()
             .domain([
-                d3.min(dataSet, data => data[dataColumn] * 0.8), 
-                d3.max(dataSet, data => data[dataColumn]) * 1.2])
+                d3.min(dataSet, data => +data[dataColumn]) * 0.8, 
+                d3.max(dataSet, data => +data[dataColumn])* 1.2])
             .range([chartHeight,0]);
 
         return scale;
@@ -84,12 +83,13 @@ d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
     };
 
     function drawCircles(xScale, yScale, dataSet){
-        dataMarkerGroup = prepareChart.selectAll("circle")
+        // plot the circles onto the chart
+       let dataMarkerGroup = prepareChart.selectAll("circle")
             .data(dataSet)
             .enter()
             .append("circle")
-            .attr("cx", d => xScale(d[xData]))
-            .attr("cy", d => yScale(d[yData]))
+            .attr("cx", d => xScale(+d[xData]))
+            .attr("cy", d => yScale(+d[yData]))
             .attr("r",10)
             .attr("fill","lightblue")
             .attr("opacity","1.0");
@@ -97,22 +97,30 @@ d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
         return dataMarkerGroup;
     };
 
-    function updateCircles(dataMarkerGroup, xData, xScale, yData, yScale) {
+    function updateCircles(dataMarkerGroup, markerText, xData, xScale, yData, yScale) {
         dataMarkerGroup.transition()
             .duration(1000)
-            .attr("cx", d => xScale(d[xData]))
-            .attr("cy", d => yScale(d[yData]));
+            .attr("cx", d => xScale(+d[xData]))
+            .attr("cy", d => yScale(+d[yData]));
+
+        markerText.transition()
+            .duration(1000)
+            .attr("x", d => xScale(+d[xData]))
+            .attr("y", d => yScale(+d[yData]));
     }
 
     function addText(xScale, yScale, dataSet) {
-        dataSet.forEach((d) => {
-            prepareChart.append("text")
-                .attr("x", xScale(d[xData]-0.45))
-                .attr("y", yScale(d[yData]-0.2))
-                .attr("fill","black")
-                .attr("style","font-size:10px")
-                .text(d.abbr)
-        });
+       let markerText = prepareChart.selectAll(null)
+            .data(dataSet)
+            .enter()
+            .append("text")
+            .attr("x", d => xScale(+d[xData]))
+            .attr("y", d => yScale(+d[yData]))
+            .attr("text-anchor","middle")
+            .attr("font-size","10px")
+            .text(d => d.abbr);
+
+        return markerText;
     };
 
     function labelAxes(xLabel, yLabel) {
@@ -140,7 +148,8 @@ d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
             .attr("y", margin.top)
             .attr("value","title")
             .attr("style","text-anchor:middle")
-            .text("Comparison of Demographics and Risk Factors in the United States")
+            .attr("font-size","20px")
+            .text("2014 Census Data Relationships (United States of America)")
     };
     function populateMenu(menu, data) {
         // generate options for the select menu, and link the associated index with the id option
@@ -152,25 +161,27 @@ d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
             dataOption.attr('value', index); 
         });
     };
- 
+    // draw the initial plot on the page
     var xScale = xAxisScale(rawData, xData);
     var yScale = yAxisScale(rawData, yData);
 
     drawAxis(xScale, yScale);
     dataMarkers = drawCircles(xScale, yScale, rawData);
-    addText(xScale, yScale, rawData);
+    markerText = addText(xScale, yScale, rawData);
     labelAxes(xLabel,yLabel);
     populateMenu("#xdata", menuLabels);
     populateMenu("#ydata", menuLabels);
+
+    // Update the chart when a change occurs on one of the select menus
     d3.selectAll("select").on("change", function() {
         // retrieve new x axis data
-        let xValue = d3.select("#xdata")
+        let xValue = d3.selectAll("#xdata")
             .property("value");
         let dataX = Object.keys(menuLabels[xValue]);
         let xLabels = Object.values(menuLabels[xValue]);
 
         //retrieve new y axis data
-        let yValue = d3.select("#ydata")
+        let yValue = d3.selectAll("#ydata")
             .property("value");
         let dataY = Object.keys(menuLabels[yValue]);
         let yLabels = Object.values(menuLabels[yValue]);
@@ -180,12 +191,7 @@ d3.csv("D3_data_journalism/data/data.csv").then(rawData => {
         d3.selectAll("#yAxisLabel").text(yLabels);
         var xScaleNew = xAxisScale(rawData,dataX);
         var yScaleNew = yAxisScale(rawData,dataY);
-
         updateAxis(xScaleNew, yScaleNew);
-        updateCircles(dataMarkers, dataX, xScaleNew,dataY, yScaleNew);
-
-
-
-        console.log(dataY)
+        updateCircles(dataMarkers,markerText, dataX, xScaleNew,dataY, yScaleNew);
     });
 });
